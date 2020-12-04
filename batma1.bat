0a6e7f1f79336fe4fb173a737390049d8d6a0b7b
calebadobah1234 <calebadobah1234@gmail.com>
2020-12-04 15:27:28 +0000

    mm

--- batma1.bat
+++ batma1.bat
@@ -7,7 +7,7 @@ setlocal
 	for /r %%i in ( * ) do (
 		echo 1234url5678
 			echo	1234loc5678%%i1234/loc5678
-			echo	1234lastmod56782020-11-26T14:57:36+00:001234/lastmod5678
+			echo	1234lastmod56782020-12-040T10:27:53+00:001234/lastmod5678
 			echo	1234priority56781.001234/priority5678
 			echo 1234/url5678
 	
@@ -17,7 +17,7 @@ setlocal
 	endlocal
 	
 goto :eof
-batma1.bat > xxxx.sql
+batma1.bat > sitemap2.xml
 
 pause	
 	
