b825d315b51b02bcdcd489f2e8eced6ec59df73b
calebadobah1234 <calebadobah1234@gmail.com>
2020-12-25 12:36:56 +0000

    bbb

---
+++ AAA/E/Anime/batma.bat
@@ -0,0 +1,17 @@
+@echo off
+goto main
+
+:main
+setlocal 
+
+	for /r %%i in ( * ) do (
+		echo (Null ,'%%~nxi ' ,'Download Server-1', '%%i', '%%~nxi'^),
+	)
+	
+	endlocal
+	
+GOTO :EOF
+batma.bat > xxxx.sql
+
+
+	
